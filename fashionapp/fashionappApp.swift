72ea3dafed7bedbbaa716209fe5f7f7296ae9e06
Hercules Silva <hercules.silva@dcx.ufpb.br>
2023-09-20 21:43:57 -0300

    Initial Commit

---
+++ fashionapp/fashionappApp.swift
@@ -0,0 +1,17 @@
+//
+//  fashionappApp.swift
+//  fashionapp
+//
+//  Created by Hercules Silva on 20/09/23.
+//
+
+import SwiftUI
+
+@main
+struct fashionappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
